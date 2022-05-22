66eb4bae185a3b1b431a5218fec8113329fb498a
Rafael Bernardino <102430354+bernardinorafael@users.noreply.github.com>
2022-05-22 12:51:09 -0300

    Add files via upload

---
+++ dark-mode/js/main.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container')
+const btn = document.querySelector('.btn')
+
+btn.onclick = function() {
+  this.classList.toggle('active')
+  container.classList.toggle('active')
+}
